5599348854c0bf3d6c843673ae62b72c2df58b46
Anh Nguyen <khacanh@gmail.com>
2012-03-17 01:02:36 +0700

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smartbox::Application.config.secret_token = '2807be3d4fe07d2918a7d303e057e65f9ff5c5fa0c0d6694ba2d90c3ac4fcd8baad3636f1fb08aa215b122ff92de988482c5b3fab728ccf40dd67664e90fb594'
